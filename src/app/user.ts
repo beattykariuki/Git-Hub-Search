72b802fabceef082b425c0ec7d75eb3f38073562
beattykariuki <bettykariuki026@gmail.com>
2018-10-30 19:15:30 +0300

    added text

---
+++ src/app/user.ts
@@ -0,0 +1,15 @@
+export class User {
+    public bio: string;
+    public name: string;
+    public location: string;
+    public followers: string;
+    public following: string;
+    public public_gists: string;
+    public public_repos: string;
+    public html_url: string;
+    public avatar_url: string;
+    public company: string;
+    public login: string ;
+    public email: string;
+    public hireable: string;
+}
